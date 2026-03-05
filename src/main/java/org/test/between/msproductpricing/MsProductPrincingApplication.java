008f7552fac999c4e12ee45cdbb11e46c91a2fd7
Robin Bray Morin <robinmorin79@gmail.com>
2026-03-05 00:43:06 -0300

    feat(technical-test): Add api first contract for get product price

---
+++ src/main/java/org/test/between/msproductpricing/MsProductPrincingApplication.java
@@ -0,0 +1,13 @@
+package org.test.between.msproductpricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductPrincingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsProductPrincingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/test/between/msproductpricing/MsProductPrincingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.between.msproductpricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductPrincingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
