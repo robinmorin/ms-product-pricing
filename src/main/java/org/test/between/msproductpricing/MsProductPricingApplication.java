41868dd3850fabe7420763cbc50a3a07f999e62e
Robin Bray Morin <robinmorin79@gmail.com>
2026-03-11 10:18:30 -0300

    feat(technical-test): Fix typo in principal class name

--- src/main/java/org/test/between/msproductpricing/MsProductPricingApplication.java
+++ src/main/java/org/test/between/msproductpricing/MsProductPricingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsProductPrincingApplication {
+public class MsProductPricingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsProductPrincingApplication.class, args);
+        SpringApplication.run(MsProductPricingApplication.class, args);
     }
 
 }
